fad0d8e7d5efdfb7d2db65fadb0594a402b50fe0
Freddy <freddyneumans@gmail.com>
2022-08-02 07:32:27 +0300

    Dockerfile после разбора ДЗ. Но не копирует файл cp hello-1.0.war /var/lib/tomcat9/webapps/

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ RUN apt install -y tomcat9
 RUN apt install -y maven
 RUN apt install -y git
 RUN git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+WORKDIR /boxfuse-sample-java-war-hello
 RUN mvn package
-RUN
-
-
+WORKDIR /target
+RUN cp hello-1.0.war /var/lib/tomcat9/webapps/
+EXPOSE 8085
+CMD ["catalina.sh", "run"]
