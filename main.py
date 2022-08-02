98d7dd1509096a84c0227f170f82e121f0a9a589
Freddy <freddyneumans@gmail.com>
2022-08-02 18:05:25 +0300

    попытка запуска images

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-FROM ubuntu:18.04
+FROM tomcat:9.0
+MAINTAINER freddy
 RUN apt-get update
-RUN apt-get install -y default-jdk
-RUN apt-get install -y tomcat9
 RUN apt-get install -y maven
 RUN apt-get install -y git
 RUN git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
-WORKDIR /boxfuse-sample-java-war-hello
+WORKDIR /usr/local/tomcat/boxfuse-sample-java-war-hello
 RUN mvn package
-WORKDIR /boxfuse-sample-java-war-hello/target
-RUN cp hello-1.0.war /var/lib/tomcat9/webapps/
-EXPOSE 8085
-CMD ["catalina.sh", "run"]
+WORKDIR /usr/local/tomcat/boxfuse-sample-java-war-hello/target
+RUN cp hello-1.0.war /usr/local/tomcat/webapps/
+EXPOSE 8080
+CMD ["catalina.sh","run"]
