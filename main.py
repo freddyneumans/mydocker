6c40ac4ad461e1bca0046e5ffec400a6b6b7f438
Freddy <freddyneumans@gmail.com>
2022-08-02 10:42:35 +0300

    осталось запустить tomcat9

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-FROM ubuntu:20.04
-RUN apt update
-RUN apt install -y default-jdk
-RUN apt install -y tomcat9
-RUN apt install -y maven
-RUN apt install -y git
+FROM ubuntu:18.04
+RUN apt-get update
+RUN apt-get install -y default-jdk
+RUN apt-get install -y tomcat9
+RUN apt-get install -y maven
+RUN apt-get install -y git
 RUN git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
 WORKDIR /boxfuse-sample-java-war-hello
 RUN mvn package
-WORKDIR /target
+WORKDIR /boxfuse-sample-java-war-hello/target
 RUN cp hello-1.0.war /var/lib/tomcat9/webapps/
 EXPOSE 8085
 CMD ["catalina.sh", "run"]
